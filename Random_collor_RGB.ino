1616723205889cc3cc82b6e72dd688129cf984be
Hugo Marques <hhugomarques@gmail.com>
2017-02-07 15:24:23 +0000

    Add files via upload

---
+++ Random_collor_RGB.ino
@@ -0,0 +1,27 @@
+//RGB LED - random colors
+
+//pin connections
+int red = 11;
+int green = 10;
+int blue = 9;
+int buttonPin = 7;
+
+void setup(){
+  pinMode(red, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+void loop(){
+  //pick a random color
+  if (digitalRead(buttonPin)==HIGH){//if button pressed
+  analogWrite(red, random(256));
+  analogWrite(blue, random(256));
+  analogWrite(green, random(256));
+  delay(15000);//wait one second
+  analogWrite(red, 0);
+  analogWrite(blue, 0);
+  analogWrite(green, 0);
+  } else {
+  }
+}
