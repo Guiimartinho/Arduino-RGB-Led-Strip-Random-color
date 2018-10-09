f6d59c52fe573ce533882cf9618b93224f2491d8
Luiz Guilherme Martinho Sampaio Ito <41489663+Guiimartinho@users.noreply.github.com>
2018-10-09 19:54:09 -0300

    Update Random_collor_RGB.ino

--- Random_collor_RGB.ino
+++ Random_collor_RGB.ino
@@ -1,4 +1,4 @@
-//RGB LED - random colors
+//RGB LED - random colors using push-button
 
 //pin connections
 int red = 11;
@@ -7,21 +7,25 @@ int blue = 9;
 int buttonPin = 7;
 
 void setup(){
-  pinMode(red, OUTPUT);
-  pinMode(blue, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(red, output);
+  pinMode(blue, output);
+  pinMode(green, output);
+  pinMode(buttonPin, input);
 }
-void loop(){
+void loop()
+{
   //pick a random color
-  if (digitalRead(buttonPin)==HIGH){//if button pressed
+  if (digitalRead(buttonPin) == HIGH){  //if button pressed
   analogWrite(red, random(256));
   analogWrite(blue, random(256));
-  analogWrite(green, random(256));
-  delay(15000);//wait one second
+  analogWrite(green, random(256))
+    
+  delay(15000);                         //wait one second
+    
   analogWrite(red, 0);
   analogWrite(blue, 0);
   analogWrite(green, 0);
-  } else {
+} else 
+    {
   }
 }
